904bba106150ddd4d61d2f2c81f02aa23834be49
Harsh Raj <87993233+HarshRaj1111@users.noreply.github.com>
2025-12-19 01:54:19 +0530

    Update AWS Cognito configuration details

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tFmwv2Lgg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1eqdu6bcrc60u9cr9vf0hfus0dd7k76vbc7367fasf697qrlo94', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_hAzy6lrTj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '47agdgjruou77a8umptb10sd3d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
