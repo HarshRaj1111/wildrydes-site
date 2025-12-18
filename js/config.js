3e1c1abf5aecf9121f22aa63dedb02b3afbed48c
Harsh Raj <87993233+HarshRaj1111@users.noreply.github.com>
2025-12-19 01:49:32 +0530

    Update Cognito userPoolId and userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_32o9KJrzt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1rvgjlbe73725vb3hedfkuvmej', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tFmwv2Lgg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1eqdu6bcrc60u9cr9vf0hfus0dd7k76vbc7367fasf697qrlo94', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
